accd4eae731a921f5eacaeb416d0693b5f948107
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-05-09 02:47:14 +0300

    refactor: standardize code formatting and add nodemon as a dev dependency

--- models/list.js
+++ models/list.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const listSchema = new mongoose.Schema({
   id: { type: Number, required: false },
   name: { type: String, required: true },
-  status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' , required: true },
-  date: { type: Date  , required: false },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+    required: true,
+  },
+  date: { type: Date, required: false },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
 });
 
-module.exports = mongoose.model('List', listSchema);
+module.exports = mongoose.model("List", listSchema);
--- models/task.js
+++ models/task.js
@@ -1,15 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String , required: false },
-  status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
-  priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Medium' },
-  date: { type: String , required: false },
+  description: { type: String, required: false },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
+  priority: { type: String, enum: ["Low", "Medium", "High"], default: "Low" },
+  date: { type: String, required: true },
   keywords: [{ type: String }],
-  listId: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
-  list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: false },
-  category: { type: String , required: true }
+  listId: { type: mongoose.Schema.Types.ObjectId, ref: "List", required: true },
+  list: { type: mongoose.Schema.Types.ObjectId, ref: "List", required: false },
+  category: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Task', taskSchema);
+taskSchema.pre("save", async function (next) {
+  if (this.isModified("listId") || this.isNew) {
+    const List = mongoose.model("List");
+    const list = await List.findById(this.listId);
+    if (list) {
+      this.category = list.name;
+    }
+  }
+  next();
+});
+
+module.exports = mongoose.model("Task", taskSchema);
